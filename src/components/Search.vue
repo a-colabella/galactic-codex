4ab5f67cbabcc9eca7c9ef200dc323b01031c92d
a-colabella <acolabella11@gmail.com>
2022-04-02 11:59:06 -0700

    home view

---
+++ src/components/Search.vue
@@ -0,0 +1,56 @@
+<script setup>
+  import IconSearch from '@/components/icons/IconSearch.vue'
+</script>
+
+<template>
+  <div class="input_wrapper">
+    <input type="text" placeholder="Search"/>
+    <button class="search-button" type="submit"><IconSearch class="search-svg"/></button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Search',
+    components: {
+      IconSearch
+    }
+  }
+</script>
+
+<style scoped>
+  .input_wrapper {
+    display: inline-block;
+    position: relative;
+    vertical-align: top;
+  }
+
+  input {
+    height: 1.5rem;
+    border-width: 1px 0 1px 1px;
+    border-radius: 2px 0 2px 2px;
+    padding: 0.8rem 9.6rem .8rem 1.2rem;
+    font-size: 1.2rem;
+    line-height: 1.2rem;
+  }
+
+  .search-button {
+    min-height: 1.6rem;
+    margin: 0;
+    border-radius: 0 2px 2px 0;
+    padding: .8rem 1.6rem;
+    font-size: 1.2rem;
+    display: inline-block;
+    cursor: pointer;
+    position: relative;
+    color: #fff;
+    background-color: #333;
+  }
+
+  .search-svg {
+    display: inline-block;
+    vertical-align: middle;
+    background-position: 0 -697px;
+    height: 1.2rem;
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
+import Search from '@/components/Search.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="title">
+    <p>Galactic Codex</p>
+  </div>
+  <div id="search-container">
+    <Search/>
+  </div>
 </template>
+
+<style scoped>
+  #search-container {
+    float: none;
+    max-width: 95%;
+    width: 540px;
+    margin: 7.4rem auto 1.95rem;
+    text-align: center;
+    vertical-align: middle;
+  }
+
+  #title {
+    float: none;
+    max-width: 95%;
+    position: relative;
+    margin: 7.4rem auto 1.95rem;
+    width: 300px;
+    font-size: 2rem;
+    text-align: center;
+  }
+
+  div {
+    display: block;
+  }
+</style>
